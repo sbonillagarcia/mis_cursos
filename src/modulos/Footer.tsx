f06ac3d180897de4ae852ce712c080195b83c03d
Katherine Bonilla <katykst_39@hotmail.com>
2024-04-16 18:42:26 -0500

    avance en el inicio

--- src/modulos/Footer.tsx
+++ src/modulos/Footer.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import { SocialIcon } from 'react-social-icons'; // Importa SocialIcon
 import './Footer.css';
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div>
-        <h4>Contacto</h4>
-        <p>Teléfono: +57 5555555</p>
-        <p>Email: katherine@gmail.com</p>
+        
+        <h4>Información de Contacto:</h4>
+          <ul> 
+          <li> 
+          Dirección: Avenida de las Palmas 345, Ciudad Esperanza, Fantasía</li>
+          <li>
+          Teléfono: +99 (555) 123-4567</li>
+          <li>
+          Correo Electrónico: info@idiomasmundo.com
+          </li>
+          </ul>
+        
       </div>
       <div>
-      <h4>Redes Sociales</h4>
+        <h4>Redes Sociales:</h4>
         <ul>
+        <li>
+          Facebook: <a href="https://facebook.com/idiomasmundo" target="_blank" rel="noopener noreferrer">facebook.com/idiomasmundo</a><br />
           <li>
-            <SocialIcon url="https://facebook.com" />
+          Twitter: <a href="https://twitter.com/idiomasmundo" target="_blank" rel="noopener noreferrer">twitter.com/idiomasmundo</a><br />
           </li>
           <li>
-            <SocialIcon url="https://youtube.com" />
+          Instagram: <a href="https://instagram.com/idiomasmundo" target="_blank" rel="noopener noreferrer">instagram.com/idiomasmundo</a><br />
           </li>
           <li>
-            <SocialIcon url="https://instagram.com" />
+          LinkedIn: <a href="https://linkedin.com/company/idiomasmundo" target="_blank" rel="noopener noreferrer">linkedin.com/company/idiomasmundo</a>
+          </li>
           </li>
         </ul>
       </div>
